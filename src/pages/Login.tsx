faa6caaa48f3adfcfe3e9d045f7a9b3e6ec9b88a
Sean <93523627+SlurpiDurpi@users.noreply.github.com>
2025-07-09 23:05:58 +0800

    feat: completed styling for signup page and added error messages

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,51 +1,93 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom";
 import CustomInputField from "../components/CustomInputField";
 import { LoginFormData } from "@/types/components";
-import { isValidEmail, isValidPassword, getEmailError, getPasswordError } from "../utils/utils";
+import {
+  isValidEmail,
+  isValidPassword,
+  getEmailError,
+  getPasswordError,
+} from "../utils/authentication";
 
-export default function Login(){
-    const navigate = useNavigate();
-    const [role, setRole] = useState<string>("Company");
-    const [loginData, setLoginData] = useState<LoginFormData>({
-        email:"",
-        password:""
-    });
-    
-    function handleLoginChange(e: React.ChangeEvent<HTMLInputElement>){
-        const name = e.target.name;
-        const value = e.target.value;
-        setLoginData((prevData) => ({...prevData, [name]: value}));
+export default function Login() {
+  const navigate = useNavigate();
+  const [role, setRole] = useState<string>("Company");
+  const [loginData, setLoginData] = useState<LoginFormData>({
+    email: "",
+    password: "",
+  });
+
+  function handleLoginChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setLoginData((prevData: LoginFormData) => ({ ...prevData, [name]: value }));
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    console.log(loginData);
+    e.preventDefault();
+    if (role == "Company") {
+      navigate("/employer/dashboard");
+    } else {
+      navigate("/employee/dashboard");
     }
+  }
 
-    function handleSubmit(e: React.FormEvent<HTMLFormElement>){
-        console.log(loginData);
-        e.preventDefault();
-        if (role == "Company"){
-            navigate("/employer/dashboard");
-        } else{
-            navigate("/employee/dashboard");
-        }
-    }
-
-    return <div className="flex flex-col py-18 gap-15"> 
-        <div className="flex flex-row gap-10 justify-center items-center">
-            <img className="" src="/public/images/optistafflogo.svg"></img>
-            <h1 className="text-4xl font-montserrat-b text-primary-blue">Your gateway to flexible hiring</h1>
-        </div>
-        <div className="px-96">
-            <div className="w-full px-7 py-15 bg-white rounded-3xl shadow-2xl">
-                <form className="w-full flex flex-col gap-8 items-center" onSubmit={handleSubmit}>
-                    <CustomInputField title="Email" name="email" type="email" onChange={handleLoginChange} valid={isValidEmail(loginData.email)} error={getEmailError(loginData.email)}/>
-                    <CustomInputField title="Password" name="password" type="password" onChange={handleLoginChange} valid={isValidPassword(loginData.password)} error={getPasswordError(loginData.password)}/>
-                    <Link className="text-center text-2xl underline" to="/forgotpassword">Forgot Password?</Link>
-                    <button className="w-full bg-primary-blue px-7 py-8 rounded-3xl text-white text-2xl cursor-pointer hover:opacity-80">Log in</button>
-                    <div className="flex flex-col items-center">
-                        <p className="text-2xl">Don't have an account yet?</p>
-                        <Link className="text-2xl underline" to="/signup">Sign Up</Link>
-                    </div>
-                </form>
+  return (
+    <div className="flex flex-col py-18 gap-15">
+      <div className="flex flex-row gap-10 justify-center items-center">
+        <img className="" src="/public/images/optistafflogo.svg"></img>
+        <h1 className="text-4xl font-montserrat-b text-primary-blue">
+          Your gateway to flexible hiring
+        </h1>
+      </div>
+      <div className="px-96">
+        <div className="w-full px-7 py-15 bg-white rounded-3xl shadow-2xl">
+          <form
+            className="w-full flex flex-col gap-8 items-center"
+            onSubmit={handleSubmit}
+          >
+            <CustomInputField
+              title="Email"
+              name="email"
+              type="email"
+              onChange={handleLoginChange}
+              valid={loginData.email == "" || isValidEmail(loginData.email)}
+              error={getEmailError(loginData.email)}
+            />
+            <CustomInputField
+              title="Password"
+              name="password"
+              type="password"
+              onChange={handleLoginChange}
+              valid={
+                loginData.password == "" || isValidPassword(loginData.password)
+              }
+              error={getPasswordError(loginData.password)}
+            />
+            <Link
+              className="text-center text-2xl font-montserrat underline hover:opacity-60"
+              to="/forgotpassword"
+            >
+              Forgot Password?
+            </Link>
+            <button className="w-full bg-primary-blue px-7 py-8 rounded-3xl text-white text-2xl cursor-pointer hover:opacity-80">
+              Log in
+            </button>
+            <div className="flex flex-col items-center">
+              <p className="text-2xl font-montserrat">
+                Don't have an account yet?
+              </p>
+              <Link
+                className="text-2xl text-primary-blue font-montserrat-smb underline hover:opacity-60"
+                to="/signup"
+              >
+                Sign Up
+              </Link>
             </div>
+          </form>
         </div>
+      </div>
     </div>
+  );
 }
