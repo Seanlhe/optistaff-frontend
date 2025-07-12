a83edc3cdab528569e6f5547c81b844a97cdaaf8
Sean <93523627+SlurpiDurpi@users.noreply.github.com>
2025-07-13 01:59:31 +0800

    feat: added more styling for landing page, sticky function for navbar

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -59,8 +59,8 @@ export default function LandingPage(){
                     <p className="font-montserrat-smb text-4xl">Create a job posting</p>
                     <p className="font-montserrat text-2xl/10">Upload your job description and event details, including manpower needs, role breakdowns, venue, time and pay at least 1 week prior.</p>
                 </div>
-                <img className="col-span-1 w-full h-auto"src="/images/dashboard.svg"/>
-                <img className="w-full h-auto"src="/images/dashboard.svg"/>
+                <img className="col-span-1 w-full h-auto max-w-125"src="/images/dashboard.svg"/>
+                <img className="w-full max-w-125 h-auto"src="/images/dashboard.svg"/>
                 <div className="col-span-1 flex flex-col gap-10">
                     <p className="font-montserrat-smb text-4xl">Receive your employee allocations</p>
                     <p className="font-montserrat text-2xl/10">Our system matches your jobs with available, qualified staff every Wednesday and Sunday at 8:30pm. <br/> <br/> In the event of cancellations, we will find suitable replacements.</p>
@@ -69,18 +69,20 @@ export default function LandingPage(){
                     <p className="font-montserrat-smb text-4xl">Execute and evaluate</p>
                     <p className="font-montserrat text-2xl/10">Track staff attendance by scanning our in-app QR code. Afterwards, leave feedback on your staff so we can continuously improve your future matches.</p>
                 </div>
-                <img className="w-full h-auto"src="/images/dashboard.svg"/>
+                <img className="w-full h-auto max-w-125"src="/images/dashboard.svg"/>
             </div>
             <div id="landing-testimonials" className="grid grid-cols-2 gap-x-37.5 px-37.5 justify-between">
-                <div className="flex flex-col gap-15 p-15 rounded-4xl rounded-bl-none bg-landing-bg">
-                    <div className="col-span-2 flex flex-row items-center">
+                <div className="relative flex flex-col gap-15 p-15 rounded-4xl rounded-bl-none bg-landing-bg">
+                    <img className="absolute -top-15 -left-10 h-32.5 w-32.5" src="/icons/quotationicon.svg"/>
+                    <div className="col-span-2 flex flex-row items-center gap-5">
                         <img className="h-20 w-20 rounded-full" src="/icons/personicon.svg" />
                         <p className="font-montserrat-smb text-2xl/10">Sean Leng<br/><span className="font-montserrat text-2xl/10">HR Lead at FinAscend</span></p>
                     </div>
                     <p className="col-span-2 font-montserrat text-2xl/10">I used to spend days working spreadsheets and advertising to get manpower. With FlexiStaff <span className="font-montserrat-smb text-2xl/10">I can upload my job details and leave it aside.</span></p>
                 </div>
-                <div className="col-span-1 flex flex-col gap-15 p-15 rounded-4xl rounded-bl-none bg-landing-bg">
-                    <div className="col-span-2 flex flex-row items-center gap-15">
+                <div className="relative col-span-1 flex flex-col gap-15 p-15 rounded-4xl rounded-bl-none bg-landing-bg">
+                <img className="absolute -top-15 -right-10 -scale-x-100 h-32.5 w-32.5" src="/icons/quotationicon.svg"/>
+                    <div className="col-span-2 flex flex-row items-center gap-5">
                         <img className="h-20 w-20 rounded-full" src="/icons/personicon.svg" />
                         <p className="font-montserrat-smb text-2xl/10">Aaron Lim<br/><span className="font-montserrat text-2xl/10">University Student</span></p>
                     </div>
