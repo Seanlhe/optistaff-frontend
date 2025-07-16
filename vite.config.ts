4defad65a184ca94f50f05e4bfd919ae4502dac2
wongkang01 <97945796+wongkang01@users.noreply.github.com>
2025-07-16 14:18:03 +0800

    fix: address to postal code lookup feature

--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,15 @@ import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  server: {
+    proxy: {
+      '/api/geocode': {
+        target: 'https://maps.googleapis.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api\/geocode/, '/maps/api/geocode'),
+        secure: true,
+      }
+    }
+  }
 })
 
